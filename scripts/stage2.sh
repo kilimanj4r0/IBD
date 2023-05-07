c1d49b03e31c74a1cdb9365c9e4e8fce8a20a77f
Vladimir Makharev <v.makharev@yandex.ru>
2023-05-07 10:39:02 +0000

    Stage II: Add HIVE tables building

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "Transfer avsc files to HDFS"
+hdfs dfs -rm -r /project/avsc
+hdfs dfs -mkdir /project/avsc
+hdfs dfs -put data/*.avsc /project/avsc
+
+echo "Transfer java files to HDFS"
+hdfs dfs -rm -r /project/java
+hdfs dfs -mkdir /project/java
+hdfs dfs -put data/*.java /project/java
+
+echo "Build HIVE tables"
+rm -rf sql/hive_results.txt
+sudo hive -f sql/db.hql > sql/hive_results.txt

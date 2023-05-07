c00d372e26537cd57870a615fe7e420b564677b2
Vladimir Makharev <v.makharev@yandex.ru>
2023-05-07 19:12:36 +0000

    Stage I and II: Update shell files to make log

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "Install git lfs and fetch data"
+echo "Install git lfs"
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
 sudo yum install git-lfs
 git lfs install
+
+echo "Fetch data from git lfs"
 git lfs fetch --all
 git lfs checkout
--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Creating DB"
-psql -U postgres -c 'DROP DATABASE IF EXISTS project;'
-psql -U postgres -c 'CREATE DATABASE project;'
-psql -U postgres -d project -f sql/db.sql
+rm -rf output/psql_results.txt
+psql -U postgres -c 'DROP DATABASE IF EXISTS project;' >> output/psql_results.txt
+psql -U postgres -c 'CREATE DATABASE project;' >> output/psql_results.txt
+psql -U postgres -d project -f sql/db.sql >> output/psql_results.txt
 
 echo "Creating a sqoop job and import all table into HDFS"
 cd data

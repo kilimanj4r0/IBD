1e643644cf0d9b9e8200f7bab870ecade31e7e06
Artem Batalov <a@batalov.me>
2023-05-10 23:09:45 +0000

    Update preprocess

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "Install git lfs"
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
-sudo yum install git-lfs
+sudo yum install -y git-lfs
 git lfs install
 
 echo "Fetch data from git lfs"
 git lfs fetch --all
 git lfs checkout
+
+echo "Modify pg_hba config"
+sed -i '1ihost all all 0.0.0.0/0 trust' /var/lib/pgsql/data/pg_hba.conf
+sed -i '1ilocal all all trust' /var/lib/pgsql/data/pg_hba.conf
